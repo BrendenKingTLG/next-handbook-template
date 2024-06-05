3a557ce07a889274a03e1f12978db1e8502be48e
BrendenKingTLG <kingbren@oregonstate.edu>
2024-06-04 21:55:16 -0500

    constants

---
+++ src/app/constants/paths.ts
@@ -0,0 +1,16 @@
+export const paths = [
+    {
+      name: "Dashboard",
+      path: "/dashboard",
+      children: [
+        {
+          name: "Overview",
+          path: "/dashboard/overview",
+        },
+        {
+          name: "Stats",
+          path: "/dashboard/stuff",
+        },
+      ],
+    },
+  ];

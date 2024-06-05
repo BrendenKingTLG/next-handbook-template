4e96635b76ddfb15c332ab3b276ccb07b90877fd
BrendenKingTLG <kingbren@oregonstate.edu>
2024-06-05 18:47:11 -0500

    page info bar

--- src/app/dashboard/overview/page.tsx
+++ src/app/dashboard/overview/page.tsx
@@ -1,36 +1,6 @@
 // pages/sections/dashboard/overview.tsx
 import React from "react";
-
-const PageInfo: React.FC = () => {
-  return (
-    <div className="overflow-x-auto h-96">
-      <table className="table table-pin-rows">
-        <thead>
-          <tr>
-            <th className="text-xl">Page Content</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Ant-Man</td>
-          </tr>
-          <tr>
-            <td>Aquaman</td>
-          </tr>
-          <tr>
-            <td>Asterix</td>
-          </tr>
-          <tr>
-            <td>The Atom</td>
-          </tr>
-          <tr>
-            <td>The Avengers</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  );
-};
+import PageInfo from "@/components/PageInfo";
 
 const Stuff: React.FC = () => {
   return (
@@ -39,7 +9,7 @@ const Stuff: React.FC = () => {
         <h1 className="text-3xl">Dashboard Stuff</h1>
         <p>This is the stuff section of the dashboard.</p>
       </div>
-      <PageInfo />
+      <PageInfo content={["x", "y", "z"]} />
     </div>
   );
 };
